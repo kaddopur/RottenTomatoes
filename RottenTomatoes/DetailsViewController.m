485a4ea7e54911e7fc13cbbaf04e6c0ca8c7e07e
Jason Huang <cjhuang@yahoo-inc.com>
2015-06-17 10:47:17 +0800

    show hi-res poster for details scene

--- RottenTomatoes/DetailsViewController.m
+++ RottenTomatoes/DetailsViewController.m
@@ -20,8 +20,21 @@
     self.titleLabel.text = self.movie[@"title"];
     self.synopsisLabel.text = self.movie[@"synopsis"];
     
-    NSURL *posterUrl = [NSURL URLWithString:[self.movie valueForKeyPath:@"posters.thumbnail"]];
+    NSURL *posterUrl = [self toHiResPosterUrl:[self.movie valueForKeyPath:@"posters.thumbnail"]];
     [self.posterView setImageWithURL:posterUrl];
+}
+
+- (NSURL *)toHiResPosterUrl:(NSString *)thumbnailUrlString {
+    // from: http://resizing.flixster.com/AzO1t1_Eo-lx0HZKeWlazwp3-Fg=/54x80/dkpu1ddg7pbsk.cloudfront.net/movie/11/19/09/11190926_ori.jpg
+    // to: http://content6.flixster.com/movie/11/19/09/11190926_ori.jpg
+
+    NSError *error = nil;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"movie.*$" options:0 error:&error];
+    NSArray *matches = [regex matchesInString:thumbnailUrlString options:0 range:NSMakeRange(0, thumbnailUrlString.length)];
+    NSString *matchText = [thumbnailUrlString substringWithRange:[matches[0] range]];
+    NSString *hiResPrefix = @"http://content6.flixster.com/";
+
+    return [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", hiResPrefix, matchText]];
 }
 
 - (void)didReceiveMemoryWarning {
